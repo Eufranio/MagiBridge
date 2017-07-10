a4a3fa7a782c0cfb19ded260e6d64d766f226214
Eufranio <franinho.anjo@live.com>
2017-07-10 17:58:35 -0300

    Added player count command, progress to #1

--- src/main/java/com/magitechserver/discord/MessageListener.java
+++ src/main/java/com/magitechserver/discord/MessageListener.java
@@ -1,12 +1,14 @@
 package com.magitechserver.discord;
 
 import br.net.fabiozumbi12.UltimateChat.UCChannel;
+import com.magitechserver.DiscordHandler;
 import com.magitechserver.MagiBridge;
 import com.magitechserver.UCHandler;
 import io.github.nucleuspowered.nucleus.modules.staffchat.StaffChatMessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.channel.MessageChannel;
 
@@ -17,39 +19,39 @@ public class MessageListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
-        String msg = null;
+
+        // Basics
+        String channelID = e.getChannel().getId();
+        String message = e.getMessage().getContent();
+        String name = e.getMember().getEffectiveName();
+        if (name == null) return;
+        if (e.getAuthor().getId() == e.getJDA().getSelfUser().getId() || e.getAuthor().isFake()) return;
+        if (message == null || message.trim().isEmpty()) return;
+        if (message.length() > 120) {
+            message = message.substring(0, message.length() - 120);
+        }
+        if (message.startsWith("```")) {
+            message = message.substring(0, message.length() - 3).substring(3);
+        }
+        if (message.startsWith("`")) {
+            message = message.substring(0, message.length() - 1).substring(1);
+        }
+        String msg = MagiBridge.getConfig().getString("messages", "discord-to-server-global-format").replace("%user%", name).replace("%msg%", message).replace("&", "§");
+
+        // UltimateChat hook active
         if (MagiBridge.getConfig().getBool("channel", "use-ultimatechat") && !MagiBridge.getConfig().getBool("channel", "use-nucleus")) {
-            String channelID = e.getChannel().getId();
             String chatChannel = MagiBridge.getConfig().getMap("channel", "ultimatechat").get(channelID);
             if (chatChannel != null) {
-                String message = e.getMessage().getContent();
-                String name = e.getMember().getEffectiveName();
 
-                // Basic checks
                 if (UCHandler.getChannelByCaseInsensitiveName(chatChannel) == null) return;
                 UCChannel channel = UCHandler.getChannelByCaseInsensitiveName(chatChannel);
-
-                if (e.getAuthor().getId() == e.getJDA().getSelfUser().getId() || e.getAuthor().isFake()) return;
-                if (message == null || message.trim().isEmpty()) return;
-                if (message.length() > 120) {
-                    message = message.substring(0, message.length() - 120);
-                }
-                if (message.startsWith("```")) {
-                    message = message.substring(0, message.length() - 3).substring(3);
-                }
-                if (message.startsWith("`")) {
-                    message = message.substring(0, message.length() - 1).substring(1);
-                }
-
-                // Apply placeholders
-                msg = MagiBridge.getConfig().getString("messages", "discord-to-server-global-format").replace("%user%", name).replace("%msg%", message);
 
                 UCHandler.sendMessageToChannel(channel, msg);
             }
         }
 
+        // Nucleus hook active
         if(MagiBridge.getConfig().getBool("channel", "use-nucleus") && !MagiBridge.getConfig().getBool("channels", "use-ultimatechat")) {
-            String channelID = e.getChannel().getId();
             MessageChannel chatChannel = null;
             if(channelID.equals(MagiBridge.getConfig().getString("channel", "nucleus", "global-discord-channel"))) {
                 chatChannel = Sponge.getServer().getBroadcastChannel();
@@ -58,31 +60,10 @@ public class MessageListener extends ListenerAdapter {
             }
 
             if (chatChannel != null) {
-                String message = e.getMessage().getContent();
-                String name = e.getMember().getEffectiveName();
-
-                // Basic checks
-                if (e.getAuthor().getId() == e.getJDA().getSelfUser().getId() || e.getAuthor().isFake()) return;
-                if (message == null || message.trim().isEmpty()) return;
-                if (message.length() > 120) {
-                    message = message.substring(0, message.length() - 120);
-                }
-                if (message.startsWith("```")) {
-                    message = message.substring(0, message.length() - 3).substring(3);
-                }
-                if (message.startsWith("`")) {
-                    message = message.substring(0, message.length() - 1).substring(1);
-                }
-
-                // Apply placeholders
-                msg = MagiBridge.getConfig().getString("messages", "discord-to-server-global-format").replace("%user%", name).replace("%msg%", message).replace("&", "§");
-
-                // Change message if on a staff channel
                 if(channelID.equals(MagiBridge.getConfig().getString("channel", "nucleus", "staff-discord-channel"))) {
                     msg = MagiBridge.getConfig().getString("messages", "discord-to-server-staff-format").replace("%user%", name).replace("%msg%", message).replace("&", "§");
                 }
 
-                // Finally sends the message
                 Text text = Text.of(msg);
                 chatChannel.getMembers().forEach(player -> player.sendMessage(text));
 
@@ -90,6 +71,23 @@ public class MessageListener extends ListenerAdapter {
                 // chatChannel.send(name, msg);
             }
         }
+
+        // Handle player list command
+        if(message.equalsIgnoreCase(MagiBridge.getConfig().getString("channel", "player-list-command"))) {
+            String players = null;
+            if(Sponge.getServer().getOnlinePlayers().size() == 0) {
+                msg = "**There are no players online!**";
+            } else {
+                for (Player player : Sponge.getServer().getOnlinePlayers()) {
+                    players = players == null ? player.getName() : players + player.getName() + ", ";
+                }
+                msg = "**Players online (" + Sponge.getServer().getOnlinePlayers().size() + "/" + Sponge.getServer().getMaxPlayers() + "):** "
+                        + "```" + players + "```";
+            }
+            DiscordHandler.sendMessageToChannel(MagiBridge.getConfig().getString("channel", "main-discord-channel"), msg);
+        }
+
+
     }
 
 }
