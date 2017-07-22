908812d22f8b8326563d3d0bdebe97d6cad6d588
Eufranio <franinho.anjo@live.com>
2017-07-22 10:39:37 -0300

    Add prefix placeholder to join/quit messages (#24)

--- src/main/java/com/magitechserver/listeners/SpongeLoginListener.java
+++ src/main/java/com/magitechserver/listeners/SpongeLoginListener.java
@@ -19,13 +19,17 @@ public class SpongeLoginListener {
             DiscordHandler.sendMessageToChannel(MagiBridge.getConfig().getString("channel", "main-discord-channel"), MagiBridge.getConfig().getString("messages", "new-players-message").replace("%player%", p.getName()));
             return;
         }
-        String joinMsg = MagiBridge.getConfig().getString("messages", "player-join-message").replace("%player%", p.getName());
+        String joinMsg = MagiBridge.getConfig().getString("messages", "player-join-message")
+                .replace("%player%", p.getName())
+                .replace("%prefix%", p.getOption("prefix").orElse(""));
         DiscordHandler.sendMessageToChannel(MagiBridge.getConfig().getString("channel", "main-discord-channel"), joinMsg);
     }
 
     @Listener(order = Order.LAST)
     public void onQuit(ClientConnectionEvent.Disconnect event, @First Player p) {
-        String quitMsg = MagiBridge.getConfig().getString("messages", "player-quit-message").replace("%player%", p.getName());
+        String quitMsg = MagiBridge.getConfig().getString("messages", "player-quit-message")
+                .replace("%player%", p.getName())
+                .replace("%prefix%", p.getOption("prefix").orElse(""));
         DiscordHandler.sendMessageToChannel(MagiBridge.getConfig().getString("channel", "main-discord-channel"), quitMsg);
     }
 
