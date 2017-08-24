9d5093b5919e95f6e70f5cf647d7dfe2df814824
Eufranio <franinho.anjo@live.com>
2017-08-24 15:11:32 -0300

    Add NucleusAPI lib and add %nick% placeholder when Nucleus is running

--- src/main/java/com/magitechserver/magibridge/listeners/SpongeChatListener.java
+++ src/main/java/com/magitechserver/magibridge/listeners/SpongeChatListener.java
@@ -5,6 +5,7 @@ import com.magitechserver.magibridge.MagiBridge;
 import com.magitechserver.magibridge.util.Config;
 import com.magitechserver.magibridge.util.GroupUtil;
 import com.magitechserver.magibridge.util.Webhooking;
+import io.github.nucleuspowered.nucleus.api.NucleusAPI;
 import io.github.nucleuspowered.nucleus.modules.staffchat.StaffChatMessageChannel;
 import nl.riebie.mcclans.channels.AllyMessageChannelImpl;
 import nl.riebie.mcclans.channels.ClanMessageChannelImpl;
@@ -25,15 +26,18 @@ public class SpongeChatListener {
     public void onSpongeMessage(MessageChannelEvent.Chat e, @Root Player p) {
         if(!Sponge.getServer().getOnlinePlayers().contains(p)) return;
         if(e.getChannel().isPresent()) {
+            String[] nick = new String[1];
             String content = e.getFormatter().getBody().toText().toPlain()
                     .replace("@everyone", p.hasPermission("magibridge.everyone") ? "@everyone" : "")
                     .replace("@here", p.hasPermission("magibridge.everyone") ? "@here" : "");
             String prefix = p.getOption("prefix").orElse("");
+            NucleusAPI.getNicknameService().ifPresent(s -> s.getNickname(p).ifPresent(n -> nick[0] = n.toPlain()));
             String format = e.getChannel().get() instanceof StaffChatMessageChannel ? MagiBridge.getConfig().getString("messages", "server-to-discord-staff-format") : MagiBridge.getConfig().getString("messages", "server-to-discord-format");
             String message = format
                     .replace("%player%", p.getName())
                     .replace("%prefix%", prefix)
                     .replace("%message%", content)
+                    .replace("%nick%", nick[0] != null ? nick[0] : "")
                     .replace("%topgroup%", GroupUtil.getHighestGroup(p));
             String discordChannel = MagiBridge.getConfig().getString("channel", "nucleus", "global-discord-channel");
 
@@ -46,6 +50,7 @@ public class SpongeChatListener {
             if(Config.useWebhooks()) {
                 Webhooking.sendWebhookMessage(MagiBridge.getConfig().getString("messages", "webhook-name")
                         .replace("%prefix%", p.getOption("prefix").orElse(""))
+                        .replace("%nick%", nick[0] != null ? nick[0] : "")
                         .replace("%player%", p.getName())
                         .replace("%topgroup%", GroupUtil.getHighestGroup(p)),
                         p.getName(),
