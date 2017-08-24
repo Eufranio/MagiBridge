7add4dfdd8f8e2acce344618480ebf55deeb98fd
Eufranio <franinho.anjo@live.com>
2017-08-24 18:58:18 -0300

    Tweak

--- src/main/java/com/magitechserver/magibridge/discord/MessageListener.java
+++ src/main/java/com/magitechserver/magibridge/discord/MessageListener.java
@@ -1,36 +1,13 @@
 package com.magitechserver.magibridge.discord;
 
-import br.net.fabiozumbi12.UltimateChat.UCChannel;
 import com.magitechserver.magibridge.DiscordHandler;
 import com.magitechserver.magibridge.MagiBridge;
 import com.magitechserver.magibridge.NucleusHandler;
 import com.magitechserver.magibridge.UCHandler;
-import com.magitechserver.magibridge.util.BridgeCommandSource;
-import com.magitechserver.magibridge.util.GroupUtil;
-import com.magitechserver.magibridge.util.ReplacerUtil;
-import flavor.pie.boop.BoopableChannel;
-import io.github.nucleuspowered.nucleus.api.NucleusAPI;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import org.spongepowered.api.Sponge;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.action.TextActions;
-import org.spongepowered.api.text.channel.MessageChannel;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.serializer.TextSerializers;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 /**
  * Created by Frani on 04/07/2017.
  */
@@ -41,7 +18,10 @@ public class MessageListener extends ListenerAdapter {
 
         // Basics
         String channelID = e.getChannel().getId();
-        String message = proccessMessage(e);
+        String message = processMessage(e);
+
+        boolean canUseColors = MagiBridge.getConfig().getString("channel", "color-allowed-role").equalsIgnoreCase("everyone") || e.getMember().getRoles().stream().anyMatch(r ->
+                r.getName().equalsIgnoreCase(MagiBridge.getConfig().getString("channel", "color-allowed-role")));
 
         if(message.isEmpty()) return;
 
@@ -50,16 +30,10 @@ public class MessageListener extends ListenerAdapter {
 
         Map<String, String> placeholders = new HashMap<>();
             placeholders.put("%user%", name);
-            placeholders.put("%msg%", message);
+            placeholders.put("%message%", canUseColors ? message : message.replaceAll("&([0-9a-fA-FlLkKrR])", "").replaceAll("§([0-9a-fA-FlLkKrR])", ""));
+            placeholders.put("%toprole%", toprole);
             placeholders.putAll(MagiBridge.getConfig().getMap("discord-to-mc-replacer"));
 
-        /*
-        String msg = ReplacerUtil.replaceEach(MagiBridge.getConfig().getString("messages", "discord-to-server-global-format")
-                .replace("%user%", name)
-                .replace("%msg%", message)
-                .replace("&", "§"),
-                MagiBridge.getConfig().getMap("discord-to-mc-replacer"));
-        */
         boolean hasAttachment = e.getMessage().getAttachments().size() >= 1;
 
         // Handle console command
@@ -70,12 +44,6 @@ public class MessageListener extends ListenerAdapter {
         // Handle player list command
         if(message.equalsIgnoreCase(MagiBridge.getConfig().getString("channel", "player-list-command")) && isListenableChannel(channelID)) {
             DiscordHandler.dispatchList(e.getMessage(), e.getChannel());
-        }
-
-        // Check if the user can use colors
-        if(!MagiBridge.getConfig().getString("channel", "color-allowed-role").equalsIgnoreCase("everyone") || e.getMember().getRoles().stream().noneMatch(r ->
-                r.getName().equalsIgnoreCase(MagiBridge.getConfig().getString("channel", "color-allowed-role")))) {
-            message = message.replaceAll("&([0-9a-fA-FlLkKrR])", "").replaceAll("§([0-9a-fA-FlLkKrR])", "");
         }
 
         // UltimateChat hook active
@@ -91,13 +59,11 @@ public class MessageListener extends ListenerAdapter {
         if(MagiBridge.getConfig().getBool("channel", "use-nucleus") && !MagiBridge.getConfig().getBool("channels", "use-ultimatechat")) {
             String format = MagiBridge.getConfig().getString("messages", "discord-to-server-staff-format");
             boolean isStaffChannel = channelID.equals(MagiBridge.getConfig().getString("channel", "nucleus", "staff-discord-channel"));
-
             NucleusHandler.handle(isStaffChannel, format, placeholders, hasAttachment, e.getMessage().getAttachments());
-
         }
     }
 
-    private static Boolean isListenableChannel(String channel) {
+    private boolean isListenableChannel(String channel) {
         if(MagiBridge.getConfig().getBool("channel", "use-ultimatechat") && MagiBridge.getConfig().getMap("channel", "ultimatechat").containsKey(channel)) {
             return true;
         }
@@ -115,7 +81,7 @@ public class MessageListener extends ListenerAdapter {
         return false;
     }
 
-    private String proccessMessage(MessageReceivedEvent e) {
+    private String processMessage(MessageReceivedEvent e) {
         String message = e.getMessage().getContent();
         if (e.getAuthor().getId().equals(e.getJDA().getSelfUser().getId()) || e.getAuthor().isFake()) return "";
         if (message == null && e.getMessage().getAttachments().size() == 0 || message.trim().isEmpty() && e.getMessage().getAttachments().size() == 0) return "";
