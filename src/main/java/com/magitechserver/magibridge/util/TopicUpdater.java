bdca8e2232a7117908cf1afac32ff990d894b553
Eufranio <franinho.anjo@live.com>
2017-09-01 19:03:38 -0300

    Add config option to disable and change the interval of the Topic Updater

--- src/main/java/com/magitechserver/magibridge/util/TopicUpdater.java
+++ src/main/java/com/magitechserver/magibridge/util/TopicUpdater.java
@@ -17,29 +17,31 @@ public class TopicUpdater extends Thread {
     @Override
     public void run() {
         while (true) {
+            if(MagiBridge.getConfig().getBool("misc", "enable-topic-updater")) {
+                String topic = MagiBridge.getConfig().getString("messages", "channel-topic-message")
+                        .replace("%players%", Integer.valueOf(Sponge.getServer().getOnlinePlayers().size()).toString())
+                        .replace("%maxplayers%", Integer.valueOf(Sponge.getServer().getMaxPlayers()).toString())
+                        .replace("%tps%", Long.valueOf(Math.round(Sponge.getServer().getTicksPerSecond())).toString())
+                        .replace("%hoursonline%", Long.valueOf(ManagementFactory.getRuntimeMXBean().getUptime() / 3600000).toString())
+                        .replace("%minutesonline%", Long.valueOf(ManagementFactory.getRuntimeMXBean().getUptime() / 60000).toString());
 
-            String topic = MagiBridge.getConfig().getString("messages", "channel-topic-message")
-                    .replace("%players%", Integer.valueOf(Sponge.getServer().getOnlinePlayers().size()).toString())
-                    .replace("%maxplayers%", Integer.valueOf(Sponge.getServer().getMaxPlayers()).toString())
-                    .replace("%tps%", Long.valueOf(Math.round(Sponge.getServer().getTicksPerSecond())).toString())
-                    .replace("%hoursonline%", Long.valueOf(ManagementFactory.getRuntimeMXBean().getUptime()/3600000).toString())
-                    .replace("%minutesonline%", Long.valueOf(ManagementFactory.getRuntimeMXBean().getUptime()/60000).toString());
+                try {
 
-            try {
+                    if (MagiBridge.jda.getTextChannelById(MagiBridge.getConfig().getString("channel", "main-discord-channel")) == null) {
+                        MagiBridge.logger.error("The main-discord-channel is INVALID, replace it with a valid one and restart the server!");
+                        continue;
+                    }
 
-                if (MagiBridge.jda.getTextChannelById(MagiBridge.getConfig().getString("channel", "main-discord-channel")) == null) {
-                    MagiBridge.logger.error("The main-discord-channel is INVALID, replace it with a valid one and restart the server!");
-                    continue;
+                    MagiBridge.jda.getTextChannelById(MagiBridge.getConfig().getString("channel", "main-discord-channel")).getManager().setTopic(topic).queue();
+
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
                 }
-
-                MagiBridge.jda.getTextChannelById(MagiBridge.getConfig().getString("channel", "main-discord-channel")).getManager().setTopic(topic).queue();
-
-            } catch (NullPointerException e) {
-                e.printStackTrace();
             }
 
+            int interval = MagiBridge.getConfig().getInt("misc", "topic-updater-interval");
             try {
-                Thread.sleep(3000);
+                Thread.sleep(interval < 5 ? 5000 : interval * 1000);
             } catch (InterruptedException e) {
                 MagiBridge.logger.error("Something interrupted the topic updater!");
             }
