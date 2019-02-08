8888dea3df10c6f699a0dec3496729ccbfc5dfd3
Daniel Widrick <daniel@widrick.net>
2019-02-08 02:38:17 +0000

    quality review changes

--- src/main/java/com/magitechserver/magibridge/VanillaHandler.java
+++ src/main/java/com/magitechserver/magibridge/VanillaHandler.java
@@ -1,24 +1,12 @@
 package com.magitechserver.magibridge;
 
-import com.magitechserver.magibridge.config.categories.Messages;
 import com.magitechserver.magibridge.util.FormatType;
 import com.magitechserver.magibridge.util.ReplacerUtil;
-import flavor.pie.boop.BoopableChannel;
-import io.github.nucleuspowered.nucleus.api.NucleusAPI;
-import net.dv8tion.jda.core.entities.Message;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.channel.MessageChannel;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.serializer.TextSerializers;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * Created by Daniel Widrick on 07/02/2018.
