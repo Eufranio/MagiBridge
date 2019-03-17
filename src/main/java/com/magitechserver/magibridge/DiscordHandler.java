efa7baae40aae3156d13eabcfe2c92bdbedcf40b
David <poq-david@live.com>
2019-03-17 05:57:23 +0330

    Removed unsed function

--- src/main/java/com/magitechserver/magibridge/DiscordHandler.java
+++ src/main/java/com/magitechserver/magibridge/DiscordHandler.java
@@ -121,17 +121,6 @@ public class DiscordHandler {
         }
     }
 
-    private static boolean isValidChannel(long channel) {
-        if (MagiBridge.jda == null) return false;
-        if (MagiBridge.jda.getStatus() != JDA.Status.CONNECTED) return false;
-        if (MagiBridge.jda.getTextChannelById(channel) == null) {
-            MagiBridge.getLogger().error("The channel " + channel + " defined in the config isn't a valid Discord Channel ID!");
-            MagiBridge.getLogger().error("Replace it with a valid one then reload the plugin!");
-            return false;
-        }
-        return true;
-    }
-
     private static boolean isValidChannel(String channel) {
         if (MagiBridge.jda == null) return false;
         if (MagiBridge.jda.getStatus() != JDA.Status.CONNECTED) return false;
